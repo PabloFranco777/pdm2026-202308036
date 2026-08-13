dc968c9050f762556d8fb27ef4b66e9ac03f2328
PabloFranco777 <129104028+PabloFranco777@users.noreply.github.com>
2026-08-13 09:29:03 -0600

    Delete Programación de Dispositivos Móviles/pdm2026-202308036 directory

--- Programación de Dispositivos Móviles/pdm2026-202308036/inkash/android/app/src/main/kotlin/com/example/inkash/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.inkash
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Programación de Dispositivos Móviles/pdm2026-202308036/parcial1/pantalla2/android/app/src/main/kotlin/com/example/pantalla2/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.pantalla2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
